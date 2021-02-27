de7ab1ac9f385050dec5906d7a88d8736742e8c9
Pedro Paulo Bazzo <pedropbazzo@outlook.com>
2021-02-27 02:08:39 -0300

    fix rm -r

--- node_modules/@nestjs/graphql/dist/interfaces/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './custom-scalar.interface';
-export * from './gql-exception-filter.interface';
-export * from './gql-gateway-module-options.interface';
-export { GqlModuleAsyncOptions, GqlModuleOptions, GqlOptionsFactory, } from './gql-module-options.interface';
